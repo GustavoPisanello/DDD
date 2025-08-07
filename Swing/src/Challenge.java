da7b10fa867e8d2555681f8c46b4eaac03eaeb1b
GustavoPisanello <gustavo.pisanello@gmail.com>
2025-08-06 22:11:39 -0300

    Desafio 05-08-25

--- Swing/src/Challenge.java
+++ Swing/src/Challenge.java
@@ -1,11 +1,9 @@
-import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
+import javax.swing.*;
 
 public class Challenge {
 
-
-    enum ComponentType {
+    public enum ComponentType {
         LABEL,
         TEXTFIELD,
         TEXTAREA,
@@ -15,46 +13,43 @@ public class Challenge {
         BUTTONGROUP
     }
 
-    public static void createElement(ComponentType componentType, String label, JPanel panel){
-        Scanner sc = new Scanner(System.in);
+    public static void createElement(ComponentType componentType, int rows, int columns, JPanel panel){
+        JComponent component = new JTextArea(rows, columns);
+        panel.add(component);
+    }
+
+    public static void createElement(ComponentType componentType, int columns, JPanel panel){
+        JComponent component = new JTextField(columns);
+        panel.add(component);
+    }
+
+    public static JComponent createElement(ComponentType componentType, String label, JPanel panel){
         JComponent component = null;
 
         switch (componentType){
             case LABEL:
                 component = new JLabel(label);
                 break;
-
-            case TEXTFIELD:
-                System.out.println("Digite o número de colunas do TextField: ");
-                int columns = sc.nextInt();
-                component = new JTextField(columns);
-                break;
-
-            case TEXTAREA:
-                System.out.println("Digite o número de linhas do TextArea: ");
-                int rows = sc.nextInt();
-
-                System.out.println("Digite o número de colunas do TextArea: ");
-                int tcolumns = sc.nextInt();
-
-                component = new JTextArea(rows, tcolumns);
-                break;
-
+            
             case CHECKBOX:
                 component = new JCheckBox(label);
                 break;
 
-            case RADIOBUTTON:
-                component = new JRadioButton(label);
-                break;
-
             case BUTTON:
                 component = new JButton(label);
+                panel.add(component);
+                return component;
         }
-
         panel.add(component);
+        return component;
     }
 
+    public static void createElement(ComponentType componentType, String label, ButtonGroup group, JPanel panel){
+        JRadioButton radio = new JRadioButton(label);
+        group.add(radio);
+        panel.add(radio);
+    }
+    
     public static void main(String[] args){
         JFrame frame = new JFrame("Formulário de contato");
         frame.setSize(400, 400);
@@ -63,47 +58,25 @@ public class Challenge {
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
 
-        JLabel nomeLabel = new JLabel("Nome: ");
-        JTextField nomeField = new JTextField(20);
+        ButtonGroup group = new ButtonGroup();
 
-        JLabel comentarioLabel = new JLabel("Comentário");
-        JTextArea comentarioArea = new JTextArea(5, 20);
-
-        JCheckBox interesseAWS = new JCheckBox("Tem interese em AWS?");
-        JRadioButton radioSim = new JRadioButton("Sim");
-        JRadioButton radioNao = new JRadioButton("Não");
-        ButtonGroup grupo = new ButtonGroup();
-        grupo.add(radioSim);
-        grupo.add(radioNao);
-
-        JButton enviarButton = new JButton("Enviar");
-
-        panel.add(nomeLabel);
-        panel.add(nomeField);
-        panel.add(comentarioLabel);
-        panel.add(comentarioArea);
-        panel.add(interesseAWS);
-        panel.add(radioSim);
-        panel.add(radioNao);
-        panel.add(enviarButton);
-
-        createElement(ComponentType.TEXTFIELD, "Digite um número: ", panel);
-        createElement(ComponentType.BUTTON, "Tem interese em AWS?", panel);
-
-
+        createElement(ComponentType.LABEL, "Nome: ", panel);
+        createElement(ComponentType.TEXTFIELD, 20, panel);
+        createElement(ComponentType.LABEL, "Comentário", panel);
+        createElement(ComponentType.TEXTAREA, 5, 20, panel);
+        createElement(ComponentType.CHECKBOX, "Tem interese em AWS?", panel);
+        createElement(ComponentType.RADIOBUTTON, "Sim", group, panel);
+        createElement(ComponentType.RADIOBUTTON, "Não", group, panel);
+        
+        JButton enviarButton = (JButton) createElement(ComponentType.BUTTON, "Enviar", panel);
 
         enviarButton.addActionListener(e -> {
-            String nome = nomeField.getText();
-            String comentario = comentarioArea.getText();
+            String nome = ((JTextField) panel.getComponent(1)).getText();
+            String comentario = ((JTextArea) panel.getComponent(3)).getText();
             System.out.println("Nome: " + nome + ", Comentário: " + comentario);
         });
-
-
 
         frame.add(panel);
         frame.setVisible(true);
     }
-
-
-
 }
